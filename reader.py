33a7ee3bc3a026cd1ad0a74b58eb8b0809678558
Specki <yushakhzod@gmail.com>
2023-08-01 11:26:52 +0300

    init: reader using tesseractOCR

---
+++ reader.py
@@ -0,0 +1,19 @@
+import os
+from pytesseract import pytesseract
+import image_enchant
+
+os.environ['TESSDATA_PREFIX'] = './tesseract-mrz'
+
+class Back:
+    def __init__(self, img):
+        self._image = img
+        self.set_MRZ_image()
+        self.set_MRZ()
+
+    def set_MRZ_image(self):
+        self._MRZ_image = image_enchant.convert_to_binary(image_enchant.get_mrz_image(self._image))
+
+    # https://github.com/DoubangoTelecom/tesseractMRZ/
+    def set_MRZ(self):
+        self.MRZ = pytesseract.image_to_string(self._MRZ_image, lang='mrz', config='--psm 6')
+
